3b95e9808bdab1024dc442f376b4d1ca61698b43
Mahesh Balla <mahesh_balla@epam.com>
2020-03-29 23:17:55 +0530

    added cors options

--- src/hometask5/hometask5.js
+++ src/hometask5/hometask5.js
@@ -11,6 +11,20 @@ const winstonLogger = require('./middleware/winstonLogger');
 const jwtValidator = require("./middleware/jwtValidator");
 
 const sequelize = require('./database/models').sequelize;
+const cors = require("cors");
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg =
+        "The CORS policy for this site does not " +
+        "allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+};
 
 const app = express();
 
